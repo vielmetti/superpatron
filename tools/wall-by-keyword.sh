7e1435287c36c86ae0bc40af11054517e1f31447
Edward Vielmetti <edward.vielmetti@gmail.com>
2013-08-12 22:35:47 -0400

    Create wall-by-keyword.sh
    wall-by-keyword: create a wall of books based on a single keyword
       todo: key phrase
       also todo: fiction, non-fiction, other special cases

---
+++ tools/wall-by-keyword.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# wall-by-keyword: create a wall of books based on a single keyword
+#   todo: key phrase
+#   also todo: fiction, non-fiction, other special cases
+
+curl 'http://www.aadl.org/catalog/search/keyword/'$1'?search_format=a|x|l&perpage=120&sort=catalog_newest&output=rss' | \
+grep "hitlist-cover" | \
+sed -e 's#<p>##' -e 's#</p>##' -e 's#/catalog#http://www.aadl.org/catalog#' 
+
