0af1fb6baf4aefbb9282343ef714c8768078545b
Edward Vielmetti <edward.vielmetti@gmail.com>
2013-08-12 23:46:34 -0400

    add example

--- tools/wall-by-keyword.sh
+++ tools/wall-by-keyword.sh
@@ -3,6 +3,7 @@
 # wall-by-keyword: create a wall of books based on a single keyword
 #   todo: key phrase
 #   also todo: fiction, non-fiction, other special cases
+# example: wall-by-keyword.sh knitting > knitting.html ; open knitting.html
 
 curl 'http://www.aadl.org/catalog/search/keyword/'$1'?search_format=a|x|l&perpage=120&sort=catalog_newest&output=rss' | \
 grep "hitlist-cover" | \
